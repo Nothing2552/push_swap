e040c0315203dd0615edaf53bf95f9345cba0287
ArzuYaprak <arzuyaprak714@gmail.com>
2026-08-26 16:15:51 +0300

    fgdfghdfgd

--- algorithms/simple.c
+++ algorithms/simple.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-static int is_sorted(t_node *a)
+static int	is_sorted(t_node *a)
 {
 	while (a && a->next)
 	{
@@ -23,68 +23,116 @@ static int is_sorted(t_node *a)
 	return (1);
 }
 
-static int find_min_index(t_node **a)
+static int	find_min_index(t_node **a)
 {
-	t_node *curr;
-	int min_val;
-	int i;
-	int min_idx;
+    t_node  *curr;
+    int     min_val;
+    int     i;
+    int     min_idx;
 
-	if (!a || !*a)
-		return (-1);
-	curr = *a;
-	min_val = __INT_MAX__;
-	i = 0;
-	min_idx = 0;
-	while (curr)
-	{
-		if (curr->value < min_val)
-		{
-			min_val = curr->value;
-			min_idx = i;
-		}
-		curr = curr->next;
-		i++;
-	}
-	return (min_idx);
+    if (!a || !*a)
+        return (-1);
+    curr = *a;
+    min_val = __INT_MAX__;
+    i = 0;
+    min_idx = 0;
+    while (curr)
+    {
+        if (curr->value < min_val)
+        {
+            min_val = curr->value;
+            min_idx = i;
+        }
+        curr = curr->next;
+        i++;
+    }
+    return (min_idx);
 }
-static int util_operations(t_node **a, t_node **b, int size, int min_idx, t_benchmark *bench)
+
+static int	util_operations(t_node **a, t_node **b, int size, int min_idx)
 {
 	int count_operations;
 
 	count_operations = 0;
-	if (min_idx <= size / 2)
-	{
-		while (min_idx > 0)
-		{
-			ra(a, bench);
+    if (min_idx <= size / 2)
+    {
+        while (min_idx > 0)
+        {
+            ra(a);
 			count_operations++;
-			min_idx--;
-		}
-	}
-	else
-	{
-		while (min_idx < size)
-		{
-			rra(a, bench);
+            min_idx--;
+        }
+    }
+    else
+    {
+        while (min_idx < size)
+        {
+            rra(a);
 			count_operations++;
-			min_idx++;
-		}
-	}
-	pb(a, b, bench);
+            min_idx++;
+        }
+    }
+    pb(a, b);
 	count_operations++;
 	return (count_operations);
 }
 
-int sort_simple(t_node **a, t_node **b, t_benchmark *bench)
+int sort_three(t_node **a)
 {
-	int size;
-	int min_idx;
-	int count_operations;
+    int count_operations;
+    int first = (*a)->value;
+    int second = (*a)->next->value;
+    int third = (*a)->next->next->value;
+
+    count_operations = 0;
+    if (first > second && first > third)
+    {
+        ra(a);
+        count_operations++;
+    }
+    else if (second > first && second > third)
+    {
+        rra(a);
+        count_operations++;
+    }
+    if ((*a)->value > (*a)->next->value)
+    {
+        sa(a);
+        count_operations++;
+    }
+    return (count_operations);
+}
+int simple_conditions(t_node **a, t_node **b, int size, int min_idx)
+{
+    int count_operations;
+
+    count_operations = 0;
+    if (size == 2)
+    {
+        sa(a);
+        return (1);
+    }
+    while (get_stack_size(*a) > 3)
+    {
+        min_idx = find_min_index(a);
+        count_operations += util_operations(a, b, get_stack_size(*a), min_idx);
+    }
+    count_operations += sort_three(a);
+    pa(a, b);
+    pa(a, b);
+    return (count_operations);
+}
+
+int	sort_simple(t_node **a, t_node **b)
+{
+    int size;
+    int min_idx;
+	int	count_operations;
 
 	count_operations = 0;
 	if (!a || is_sorted(*a))
 		return (0);
+<<<<<<< HEAD
 	while (!is_empty(a))
 	{
 		size = get_stack_size(*a);
@@ -98,7 +146,23 @@ int sort_simple(t_node **a, t_node **b, t_benchmark *bench)
 	while (!is_empty(b))
 	{
 		pa(a, b, bench);
+=======
+    while (!is_empty(a))
+    {
+        size = get_stack_size(*a);
+        min_idx = find_min_index(a);
+        if (size <= 5)
+        {
+            return (simple_conditions(a, b, size, min_idx));
+        }
+		count_operations += util_operations(a, b, size, min_idx);
+    }
+    while (!is_empty(b))
+    {
+        pa(a, b);
+>>>>>>> 53e7a20 (deneme)
 		count_operations++;
-	}
+    }
 	return (count_operations);
 }
+
